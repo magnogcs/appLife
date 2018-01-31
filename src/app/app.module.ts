6629968e4a3ec625888887758215225ab9cf45f7
Magno Guimarães <magnogcs@gmail.com>
2018-01-31 20:52:13 -0300

    v1

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { TabsPage } from '../pages/tabs/tabs';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { ApiProvider } from '../providers/api/api';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    TabsPage,
+    AboutPage,
+    ContactPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ApiProvider
+  ]
+})
+export class AppModule {}
